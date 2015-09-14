233ebe213d666020ee426dec483dc08622dd1701
V1C0D3R <V1C0D3R@gmail.com>
2015-09-14 02:37:15 +0200

    Fix menu

--- Patterns.ino
+++ Patterns.ino
@@ -51,6 +51,112 @@ void littleLight(int milli, int noComeback, int bounce)  {
   delay(milli);
 }
 
+/**
+* Draw the border of a square, beginning to the given coordonate
+* The x variable is fixed for the sqaure to be drawn on the y,z plan
+*/
+void xSquareBorder(int milli, int xStart, int yStart, int zStart, int squareSize) {
+
+  // TODO : Are the corner displayed twice ? Think about it. Later... :D. Sure they do x)
+
+  int x = xStart,
+      y = yStart,
+      z = zStart;
+
+  // draw the first side of the border
+  // y iterates (increasing) over the width, z is fixed
+  for (; y < yStart + squareSize -1; ++y){
+    displayCoord(x, y, z);
+    delay(milli);
+  }
+  
+  // draw the second side of the border
+  // y is fixed, z iterates (increasing) over the width
+  for (; z < zStart + squareSize -1; ++z){
+    displayCoord(x, y, z);
+    delay(milli);
+  }
+
+  // draw the third side of the border
+  // y iterates (decreasing) over the width, z is fixed
+  for (; y > yStart; --y){
+    displayCoord(x, y, z);
+    delay(milli);
+  }
+
+  // draw the fourth side of the border
+  // y is fixed, z iterates (decreasing) over the width
+  for (; z > zStart; --z){
+    displayCoord(x, y, z);
+    delay(milli);
+  }
+}
+
+/**
+* @author Touzard Loïc loic.touzard@gmail.com
+*
+* Fill a "layer" with a spiral by default the x = 0 layer, y and z may change to draw the spiral
+* The draw start at point (0,0,0) then iterate over +y, +z, -y, -z
+*
+* Algorithm : The spiral is drawn by drawing squares' borders.
+*   If fromOuterToInner is true :
+*     Starting with a 8-width square (the outer one), drawing its border
+*     Then drawing the inner 6-width square's border (8 minus 2 from the border)
+*     And so forth ...
+*   Else the algorithm is the same but beginning with the smaller square 
+*/
+void spiralTest(int milli, bool fromOuterToInner) {
+
+  // OPTIMIZATION, use 'byte' variable since x, y, z, squareSize are constrainted between 0 and 8, 'byte' is widely enougb
+
+  // firstly, initialyze the x coordonates
+  int startPoint, squareSize;
+
+  if (!fromOuterToInner)
+  {
+    // if the drawing direction start from the inside, change the startPoint to be the innermost square
+   
+    startPoint = (LEDS_PER_ROW%2 == 0) ? LEDS_PER_ROW/2-1 : LEDS_PER_ROW/2;
+    //startPoint = LEDS_PER_ROW / 2 - 1 + (LEDS_PER_ROW % 2);
+    // smaller width possible : 1 or 2
+    // squareSize = (LEDS_PER_ROW%2 == 0) ? 2 : 1;
+    squareSize = 2 - (LEDS_PER_ROW % 2);
+
+    if (squareSize == 1){
+      // the first point for the 1-width square
+      displayCoord(LEDS_PER_ROW-1-startPoint, startPoint, startPoint);
+      delay(milli);
+      ++squareSize;
+      --startPoint;
+    }
+
+    // Let's draw the current Square's border
+    for (; squareSize <= LEDS_PER_ROW; squareSize+=2){
+      xSquareBorder(milli, LEDS_PER_ROW-1-startPoint, startPoint, startPoint, squareSize);
+      //set the new start for the next spiral
+      --startPoint;
+    }
+
+  }
+  else{
+    squareSize = LEDS_PER_ROW;  // Size of the outer square
+    startPoint = 0; // spiral will start at (0,0,0)
+
+    // Let's draw the current Square's border
+    for (; squareSize > 1; squareSize-=2){
+      xSquareBorder(milli, startPoint, startPoint, startPoint, squareSize);
+      //set the new start for the next spiral
+      ++startPoint;
+    }
+
+    if (squareSize == 1){
+      // one last point for the 1-width square
+      displayCoord(startPoint, startPoint, startPoint);
+      delay(milli);
+    }
+  }
+}
+
 void LeapDraw()  {
     for(int i=0;i<20; i++)  {
     if(coloring[i]==0)
