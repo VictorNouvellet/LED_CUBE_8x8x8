d0ca28c08962093809c1bdcb877c2c652fffbc3b
Loïc Touzard <ltouzard@degustandco.com>
2015-09-15 11:00:51 +0200

    fix and change to 60fps to prevent sparkling

--- Patterns.ino
+++ Patterns.ino
@@ -164,9 +164,9 @@ void allAtOnce() {
   // The total quantity of LEDS
   int LEDSnum = LEDS_PER_ROW * LEDS_PER_ROW * LEDS_PER_ROW;
   // The number of fps required "frame per second"
-  int fps = 30;
+  int fps = 60;
   // The delta between 2 LED display to fit the fps requirements (in micro-second)
-  int deltaLED = 1000000. * LEDSnum / fps;
+  int deltaLED = 1000000. / LEDSnum / fps;
   for (int i = 0; i < (LEDSnum); ++i)
   {
     displayNum(i);
